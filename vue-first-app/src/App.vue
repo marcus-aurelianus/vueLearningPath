3aa355a9f0cb7fd2dd88b7c1805469841a1b38e5
Marcus Aurelianus <37787424+marcus-aurelianus@users.noreply.github.com>
2020-11-11 16:07:43 +0800

    vue3

---
+++ vue-first-app/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      friends: [
+        {
+          id: "manuel",
+          name: "Manuel Lorenz",
+          phone: "0123 45678 90",
+          email: "manuel@localhost.com",
+        },
+        {
+          id: "julie",
+          name: "Julie Jones",
+          phone: "0987 654421 21",
+          email: "julie@localhost.com",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
